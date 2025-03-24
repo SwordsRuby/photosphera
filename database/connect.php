1748a681ff68c98d15130e67bca9a1a8c0ccf005
SwordsRuby <129753520+SwordsRuby@users.noreply.github.com>
2025-03-24 23:00:08 +0300

    Delete database directory

--- database/connect.php
+++
@@ -1,9 +0,0 @@
-<?
-try {
-    $dsn = 'mysql:host=MySQL-8.2;dbname=photospheraDB;charset=utf8;';
-    $DB_username = 'root';
-    $DB_password = '';
-    $sql = new PDO($dsn, $DB_username, $DB_password);
-} catch (PDOException $e) {
-    die($e->getMessage());
-}
