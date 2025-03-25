7b772460e56481945392e0ad4536936a6eba9422
SwordsRuby <129753520+SwordsRuby@users.noreply.github.com>
2025-03-25 16:37:53 +0300

    Delete photosphera directory

--- photosphera/js/main.js
+++
@@ -1,8 +0,0 @@
-function burger_menu() {
-    const burger_el = document.getElementsByClassName('burger');
-    const burger_menu = document.getElementsByClassName('burger-menu');
-
-    burger_el[0].classList.toggle('none');
-    burger_menu[0].classList.toggle('none');
-    burger_menu[1].classList.toggle('burger-active');
-}
